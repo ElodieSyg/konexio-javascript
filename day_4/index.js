923acb77a130048b473e96338de10acf79418fa9
Elodie Sayavong <sayavong.pro@gmail.com>
2021-06-17 17:31:59 +0200

    Day 03 javascript

--- day_4/index.js
+++ day_4/index.js
@@ -59,11 +59,62 @@ function compare (num1, num2) {
 // 05 - Add up // 
 
 function addUp (num) {
-    for (var i = 1; i <= num; i++) {
+    var limit = num;
+    for (var i = 1; i < limit; i++) {
         num = num + i;
     }
-    console.log(num)
+    // console.log(num)
 }
 
 addUp(12)
+
+// 06 - Time //
+
+function format (num) {
+    let hours = (num / 3600) // 3600 = nombre de secondes dans une heure
+    let hoursTotal = Math.floor(hours)
+    num %= 3600;
+    let minutes = Math.floor(num / 60) // 60 = minutes dans une heure
+    let second = num % 60;
+
+    //console.log(hoursTotal, ":", minutes,":", second,":");
+    console.log(`${hoursTotal}:${minutes}:${second}`)
+}
+
+format(4550)
+
+// Bonus 01 //
+
+function generatePassword (num) {
+    if ( num < 6 || num > 15 ) {
+        console.log("Error")
+        return;
+    }   
+
+    var string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var stringLength = num;
+    var randomString = "";
+
+    for (var i = 0; i < stringLength; i++) {
+        var index = Math.floor(Math.random() * string.length);
+        randomString += string.charAt(index);
+    }
+    console.log(randomString);
+}
+
+generatePassword(8)
+
+// Bonus 02 //
+
+function launchDice (numberOfDice) {
+    var dice = [1, 2, 3, 4, 5, 5];
+
+
+    for ( var i = 0; i < numberOfDice; i++ ) {
+        var random = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+        console.log(random);
+    }
+return;
+}
+    launchDice(6)
 
